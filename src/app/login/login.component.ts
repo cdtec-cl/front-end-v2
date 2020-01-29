9792ff319d057c30213311bf9a0da9248b661e89
Admin CDTEC <53204168+cdtec-chile@users.noreply.github.com>
2020-01-29 00:35:32 -0300

    Update login.component.ts
    OJO con esto, debe validar usuarios desde la base de datos en vez de hardcode

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -60,7 +60,8 @@ export class LoginComponent implements OnInit,OnDestroy,AfterViewInit {
 		const target = event.target;
 		const usuario= target.querySelector('#usuario').value;
 		const password= target.querySelector('#password').value;
-	  
+	 
+		//JLA: OJO esto debe validarse desde la base de datos y no estar quemado en el código
 		if (usuario == 'Admin' && password == '12345678') {
 			console.log('entre');
 			this._router.navigate(['/dashboard']);
